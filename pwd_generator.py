51444c24c8abe72d8754224cf8a7a679a57f117c
Sultan Bazher <107263975+SultanCYB@users.noreply.github.com>
2023-05-10 07:45:26 +0300

    Cleaning and updating the code

--- pwd_generator.py
+++ pwd_generator.py
@@ -8,7 +8,7 @@ import random
 def pwd_generator(length=0, num=True, symbol=True):
     '''This function has 3 optional parameters :
 
-	- length : length of the password must be at least 8 characters, there is no max range,
+	- length : length of the password,there is no max range,
 	if no length given; the function will choose a random length between 8 and 40
 
 	- num : if False given; it will exclude digits from the password
@@ -32,36 +32,16 @@ def pwd_generator(length=0, num=True, symbol=True):
         symbol = list(punctuation)
         choices.append(symbol)
 
-    else:
-        # if the user didnt want symbols
-        symbol = []  # this was declared to prevent errors
-
     if not length:
         # if no length given it will give a random length between 8-40
 
 
         length = random.choice(range(8,41))
-
-    elif length < 8:
-        return "Length must be at least 8"
-
+    password += random.choice(random.choice(letters))
+    
     while len(password) != length:
 
-        if not password:
-            # this will always make the password starts with letters
-            password += random.choice(random.choice(letters))
+        check = random.choice(random.choice(choices))
 
-        else:
-            check = random.choice(random.choice(choices))
-
-            last_char = password[-1].lower()
-
-            # this will prevent duplicate character
-            if check.lower() != last_char:
-
-                # this will prevent two symbols being after each other respectively
-                if check not in symbol or last_char not in symbol:
-
-                    password += check
-
+        
     return password
